92378f548f44298581cde5d871d7f83e2729c987
Xie <z.xie4@umiami.edu>
2020-02-18 17:33:01 -0600

    go home

--- RNNGrid.py
+++ RNNGrid.py
@@ -13,16 +13,19 @@ class RNNGridLayer(nn.Module):
             6   7   8
         """
         super().__init__()
-        moves = (distance * 2 + 1) ** 2
+        self.dist = distance
+        self.epsilon = 1e-9
+        moves = distance * 2 + 1
+        movesq = moves**2
         self.conv_ih = Conv2d(in_channels, 3*num_hidden, kernel_size, padding=kernel_size//2)
         self.conv_hh = Conv2d(num_hidden, 3*num_hidden, 1)
-        self.conv_ho = Conv2d(num_hidden, moves, 1)
-        self.conv_oo = Conv2d(moves, moves, 1, bias=False)
-        self.merge_kernel = torch.zeros(1, moves, kernel_size, kernel_size, requires_grad=False)
-        for i in range(moves):
-            height = i / kernel_size
+        self.conv_ho = Conv2d(num_hidden, movesq, 1)
+        self.conv_oo = Conv2d(moves, movesq, 1, bias=False)
+        self.merge_kernel = torch.zeros(1, movesq, moves, moves, requires_grad=False)
+        for i in range(movesq):
+            height = i // kernel_size
             width = i % kernel_size
-            self.merge_kernel[0, moves-i, height, width] = 1
+            self.merge_kernel[0, movesq-i-1, height, width] = 1
 
     def forward(self, x, hidden):
         # taking from https://github.com/pytorch/benchmark/blob/master/rnns/fastrnns/cells.py
@@ -33,6 +36,9 @@ class RNNGridLayer(nn.Module):
         newgate = torch.tanh(i_n + resetgate * h_n)
         hy = newgate + inputgate * (hidden - newgate)
         o = torch.softmax(self.conv_ho(hy), 1)
-        torch.einsum("bcwh, bdwh -> bcdwh", x, o).view(x.shape[0], o.shape[1]*x.shape[1], x.shape[2], x.shape[3])
+        merged_o = conv2d(o, self.merge_kernel, padding=self.dist) + self.epsilon
+        hxo = torch.einsum("bcwh, bdwh -> bcdwh", x, o).view(x.shape[0], o.shape[1]*x.shape[1], x.shape[2], x.shape[3])
+        merged_h = conv2d(hxo, )
+
 
 
